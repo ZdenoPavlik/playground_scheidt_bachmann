a10b8b0ee4f8676fd13926e99ff05091c8f8f4b1
Zdeno Zdeno <zdenozdenozdeno69@gmail.com>
2025-03-18 13:42:11 +0100

    Added gitignore file

---
+++ bbuild.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+reset
+buildDir="build"
+
+echo '************************************'
+echo '******        Building        ******'
+echo '************************************'
+
+if [ ! -d "$buildDir" ]; then
+    echo '************************************'
+    echo '****** Build dir not yet present, starting initial configuration ******'
+    echo '************************************'
+    sh ./rebuild.sh
+else
+    echo '************************************'
+    echo '******   Already configured   ******'
+    echo '************************************'
+fi
+
+cd build
+make -j 16
+
+echo '************************************'
+echo '******          Done          ******'
+echo '************************************'
