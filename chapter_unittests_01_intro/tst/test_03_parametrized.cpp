c374a0162e144b965f750cbd5c32c0865805f4d9
ZdenoPavlik <pavlik.zdenko@scheidt-bachmann.sk>
2026-09-15 10:02:09 +0200

    wip

--- chapter_unittests_01_intro/tst/test_03_parametrized.cpp
+++ chapter_unittests_01_intro/tst/test_03_parametrized.cpp
@@ -17,7 +17,8 @@ TEST_P(CalculatorTest_03, addition_positive)
 	EXPECT_EQ(calc.add(number, number), number + number);
 }
 
-INSTANTIATE_TEST_SUITE_P(Addition,	   // Prefix name for this instantiation
-	CalculatorTest_03,				   // Test fixture class name
-	::testing::Values(2, 4, 6, 8, 2.3) // List of parameters to test
+INSTANTIATE_TEST_SUITE_P(Addition,												// Prefix name for this instantiation
+	CalculatorTest_03,															// Test fixture class name
+	::testing::Values(2, 4, 6, 8, 2.3, 10, 11, 12, (double)(2.4), (uint8_t)(1)) // List of parameters to test. Supports heterogenous values!
+
 );
