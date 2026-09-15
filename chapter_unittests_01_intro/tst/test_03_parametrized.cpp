2ee457d6f7a1836d091d0a635680a478bebc2da7
ZdenoPavlik <pavlik.zdenko@scheidt-bachmann.sk>
2026-09-15 13:10:24 +0200

    wip

--- chapter_unittests_01_intro/tst/test_03_parametrized.cpp
+++ chapter_unittests_01_intro/tst/test_03_parametrized.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include <memory>
+#include <iostream>
 
 #include "calculator.hpp"
 
@@ -12,13 +12,12 @@ public:
 
 TEST_P(CalculatorTest_03, addition_positive)
 {
-	int number = GetParam(); // Retrieve the current parameter value
+	auto number = GetParam(); // Retrieve the current parameter value
 	std::cout << "Testing sum of " << number << " and " << number << std::endl;
 	EXPECT_EQ(calc.add(number, number), number + number);
 }
 
-INSTANTIATE_TEST_SUITE_P(Addition,												// Prefix name for this instantiation
-	CalculatorTest_03,															// Test fixture class name
-	::testing::Values(2, 4, 6, 8, 2.3, 10, 11, 12, (double)(2.4), (uint8_t)(1)) // List of parameters to test. Supports heterogenous values!
-
+INSTANTIATE_TEST_SUITE_P(Addition,			  // Prefix name for this instantiation
+	CalculatorTest_03,						  // Test fixture class name
+	::testing::Values(2, 4, 6, 8, 10, 11, 12) // List of int parameters for TestWithParam<int>
 );
---
+++ chapter_unittests_01_intro/tst/test_04_additional.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+
+#include "calculator.hpp"
+
+void segfault()
+{
+	std::cerr << "Memory access violation" << std::endl;
+
+	int* pointer = nullptr;
+	*pointer = 5;
+}
+
+TEST(CalculatorTest, Additional)
+{
+	Calculator calc;
+	EXPECT_EQ(calc.add(2, 3), 5);
+
+	EXPECT_TRUE(calc.add(2, 3) > 1);
+	ASSERT_TRUE(calc.add(2, 3) > 1);
+
+	EXPECT_GT(calc.add(2, 3), 1); // Greater than
+	EXPECT_LT(calc.add(2, 3), 6); // Less than
+
+	EXPECT_ANY_THROW({ throw std::exception(); });
+	EXPECT_DEATH(segfault(), "Memory access violation");
+}
